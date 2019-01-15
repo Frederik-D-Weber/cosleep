7681af93ff0739c7530d4fc092adb60fdbf6f17a
Frederik_D_Weber <research@frederikweber.com>
2019-01-15 13:03:51 +0100

    install script on ubuntu reformatted

--- deploy/configure_ubuntu.sh
+++ deploy/configure_ubuntu.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # tested in Ubuntu 18.04 LTS
-# required setup for cosleep to work
-sudo apt-get -y install linux-lowlatency
-sudo apt-get -y install sox
-sudo apt-get -yes install libsox-fmt-mp3
+# suggested absolute minimal install
 ## workaround for permission of recorder_simulation to the ttyUSB*
 sudo chmod 666 /dev/ttyUSB*
 sudo usermod -a -G dialout $USER
 
-# recommended setup
+# additional extension for better use of cosleep to work
+sudo apt-get -y install linux-lowlatency
+sudo apt-get -y install sox
+sudo apt-get -yes install libsox-fmt-mp3
+
+# recommended extensions that make the work easier on the system
 sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 sudo apt-get update
 sudo apt-get -y install grub-customizer
@@ -24,10 +26,10 @@ sudo apt-get -y install python-pip
 sudo apt-get -y install python-qt4
 sudo apt-get -y install python-alsaaudio
 
-# to run from Files by double clicking
+## to run from Files by double clicking
 sudo apt-get -y install nautilus-actions # might not work
 
-# to configure and check sound card properties
+## to configure and check sound card properties
 sudo apt-get -y install jackd2
 
 ## to track the CPU usage in a GUI
